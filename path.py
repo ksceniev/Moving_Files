034942c1fd40ca730c33bc17ebb115a5119e5079
kanjiceniev <55764096+kanjiceniev@users.noreply.github.com>
2020-03-29 18:50:13 -0300

    Create path.py

---
+++ path.py
@@ -0,0 +1,24 @@
+import os
+
+documents = ['txt', 'doc', 'pdf']
+pictures = ['png', 'jpg', 'jpeg']
+music = ['mp3', 'mp4', 'wav']
+videos =  ['webm']
+
+#os.chdir('/home/annie/Downloads')
+files = os.listdir('/home/annie/Downloads')
+
+for file in files:
+    splited_file = file.split('.')
+    
+    if splited_file[-1] in pictures:
+        os.system("mv {} /home/annie/Pictures".format(file))
+    
+    elif splited_file[-1] in documents:
+        os.system("mv {} /home/annie/Documents".format(file))
+    
+    elif splited_file[-1] in music:
+        os.system("mv {} /home/annie/Music".format(file))
+    
+    elif splited_file[-1] in videos:
+        os.system("mv {} /home/annie/Videos".format(file))
